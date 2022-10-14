918fc013c9da805701f189ecffadf6ea98bdac50
wenchi0908 <88484958+wenchi0908@users.noreply.github.com>
2022-10-14 22:02:16 +0800

    Update deploy_local.sh

--- deploy_local.sh
+++ deploy_local.sh
@@ -18,21 +18,9 @@ while getopts 'bf' OPTION; do
             ;;
     esac
 done
-if [ $build_b -eq 0 ] && [ $build_f -eq 0 ]
-then
-    build_b=1
-    build_f=1
-fi
-
-# clean folder
-rm -rf ./front_src/
-mkdir -p ./front_src/
-#rm -rf ./front_src/build/
-echo "[ Init ./front_src/ ]"
 
 # stop all cintainers
 docker-compose down
-#docker volume rm 0823_2022_test_vol 
 
 # build image
 echo "[ Images building ... ]"
@@ -47,16 +35,15 @@ then
     echo "[ Build backend ]"
     docker-compose build --no-cache www
 fi
-#docker-compose build frontend-web www
 echo "[ Images builded ]"
-
-# package static source in container:frontend-web
-docker-compose up frontend-web
-echo "[ Static source created in container:frontend-web ]"
 
 # run app in container:www
 docker-compose up -d www
-docker-compose exec -itd www sh /APP/deploy.sh
+#docker-compose exec -itd www sh /APP/deploy.sh
 echo "[ App is running in container:www ]"
-docker-compose ps
 
+# package static source in container:frontend-web
+docker-compose up -d frontend-web
+echo "[ Static source created in container:frontend-web ]"
+
+docker-compose ps
